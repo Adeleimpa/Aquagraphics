53408387062da959deb8f86a8bcb9c81d03fac7f
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2023-11-24 10:52:39 +0100

    remove texture option

--- Code/waterRendering/Plane.h
+++ Code/waterRendering/Plane.h
@@ -164,69 +164,6 @@ public:
     }
 
 
-    void addHeightMap(unsigned char *HM_data, int height_HM, int width_HM){
-        double max = 1.0; // maximum height
-        double min = 0.0; // minimum height
-        // TODO adapt to any max and min value -> shader
-
-        int height_plane = h + 1;
-        int width_plane = w + 1;
-
-        int range_ndg_HM = 256;
-
-        //int count = 0;
-        //int line = 0;
-        for(int i = 0; i < indexed_vertices.size(); i++){
-
-            /*if(count%10 == 0){
-                line++;
-                std::cout << "LINE " << line << std::endl;
-            }
-            count++;*/
-
-            //int dat = (int) HM_data[i]
-            int row = floor(i / height_plane);
-            float row_plane = (float)row/(float)height_plane;
-            int row_HM = floor(height_HM*row_plane);
-            int col = i - row*width_plane;
-            float col_plane = (float)col/(float)width_plane;
-            int col_HM = floor(width_HM*col_plane);
-            int dat = (int) HM_data[row_HM*width_HM + col_HM];
-            //std::cout << "data HeightMap :" << dat << std::endl;
-
-            double ratio = (double)dat/(double)range_ndg_HM;
-            //std::cout << "ratio :" << ratio << std::endl;
-            double difference = max*ratio;
-            //std::cout << "difference :" << difference << std::endl;
-
-            indexed_vertices[i][1] = max - difference;
-        }
-    }
-
-    double getHeightFromCoords(unsigned char *HM_data, int height_HM, int width_HM, glm::vec3 coords){
-
-        // TODO doesn't work for bottom_right and top_right corners!
-
-        double dist_from_zero_x = center[0] - (width/2.0);
-        double ratio_x = (coords[0] - dist_from_zero_x)/width;
-
-        int row_HM = floor(ratio_x*width_HM);
-
-        double dist_from_zero_z = center[2] - (height/2.0);
-        double ratio_z = (coords[2] - dist_from_zero_z)/height;
-
-        int col_HM = floor(ratio_z*height_HM);
-
-        int dat = (int) HM_data[row_HM*width_HM + col_HM];
-
-        int range_ndg_HM = 256;
-        double max = 1.0; // maximum height
-        double ratio = (double)dat/(double)range_ndg_HM;
-        double difference = max*ratio;
-
-        return max - difference;
-    }
-
 
 };
 
