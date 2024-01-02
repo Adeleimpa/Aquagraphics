2e08f4b5a9b4be446110d0d0c0a744fc3c86c27a
Adele <56159423+Adeleimpa@users.noreply.github.com>
2024-01-03 00:24:39 +0100

    remove depth buffer

--- Code/waterRendering/main.cpp
+++ Code/waterRendering/main.cpp
@@ -291,14 +291,6 @@ int main( void )
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, reflectionTexture, 0);
-    //----
-    GLuint depthBuffer;
-    glGenRenderbuffers(1, &depthBuffer);
-    glBindRenderbuffer(GL_RENDERBUFFER, depthBuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuffer);
-    //---
-    glEnable(GL_DEPTH_TEST);
     //---
     GLint reflectionTextureLocation = glGetUniformLocation(programID, "reflectionTexture");
 
